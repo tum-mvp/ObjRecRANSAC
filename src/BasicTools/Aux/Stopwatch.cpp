69e48b5d43821c684f98ebbf7e07a6d74ef6cf2c
Andrew Hundt <ATHundt@gmail.com>
2015-12-28 18:44:23 -0500

    stopwatch supports OS X

--- src/BasicTools/Aux/Stopwatch.cpp
+++ src/BasicTools/Aux/Stopwatch.cpp
@@ -7,8 +7,22 @@
 
 #include "Stopwatch.h"
 
+#ifdef __APPLE__
+#include "realtime.hpp"
+#endif // __APPLE__
+
 Stopwatch::Stopwatch(bool print_err_msg)
 {
+#ifdef __APPLE__
+#ifdef HAVE_REALTIME
+        int msToNS = 1000000, period = 1*msToNS, computation = 0.1*msToNS, constraint = 0.5*msToNS;
+         bool preemptible = false;
+        if(set_realtime( period,  computation,  constraint,  preemptible)){
+           std::cout << "realtime mode successfully enabled!\n";
+        };
+#endif // HAVE_REALTIME
+#else // __APPLE__
+    // LINUX
 	// Measure test time
 	sched_param mySchedParam;
 	mySchedParam.__sched_priority =	sched_get_priority_max(SCHED_FIFO);
@@ -19,7 +33,6 @@ Stopwatch::Stopwatch(bool print_err_msg)
 			printf("Stopwatch::Stopwatch(): sched_get_priority_max() failed\n");
 		mySchedParam.__sched_priority = 99;
 	}
-
 	if ( sched_setscheduler(0, SCHED_FIFO, &mySchedParam) == -1 && print_err_msg )
 	{
 		printf("Stopwatch::Stopwatch(): Changing to real-time priority level %i failed. ", mySchedParam.__sched_priority);
@@ -41,7 +54,7 @@ Stopwatch::Stopwatch(bool print_err_msg)
 				break;
 		}
 	}
-
+#endif
 	fflush(stdout);
 }
 
